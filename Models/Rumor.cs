44626cf713f9e3186334dea78dd211c8aa9e6fa8
Y_Zhong <zhongyuan12345@qq.com>
2020-07-16 17:10:26 +0800

    增加舆情实体类

--- Models/Rumor.cs
+++ Models/Rumor.cs
@@ -1,11 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace WebApi.Models
 {
-    public class Rumor
+    public class Rumor : ICloneable
     {
+        [Key]
+        public long RumorId { get; set; }
+        public string Title { get; set; }
+        public string MainSummary { get; set; }
+        public int RumorType { get; set; }
+        public string Body { get; set; }
+        public long CrawlTime { get; set; }
+        public string Date { get; set; }
+
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
     }
 }
