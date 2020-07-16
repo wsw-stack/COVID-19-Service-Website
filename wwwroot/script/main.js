c5f02e5fa7a99ba10bbef7648ea2b4db63fa1c49
PeterSH6 <13302211862@189.cn>
2020-07-16 19:35:36 +0800

    no message

--- wwwroot/script/main.js
+++ wwwroot/script/main.js
@@ -3,10 +3,27 @@
 var Main = {
     data() {
         return {
-            fit: 'contain',
-            url: 'img/WHU.png'
+            activeIndex: '1',
+            activeIndex2: '2'
+        };
+    },
+    methods: {
+        handleSelect(key, keyPath) {
+            console.log(key, keyPath);
         }
     }
 }
 var Ctor = Vue.extend(Main)
 new Ctor().$mount('#app')
+
+//var Main = {
+//    data() {
+//        return {
+//            fit: 'contain',
+//            url: 'img/WHU.png'
+//        }
+//    }
+//}
+//var Ctor = Vue.extend(Main)
+//new Ctor().$mount('#app')
+
