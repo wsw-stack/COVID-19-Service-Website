94f9db102e3f8a85b46f89374964d7271b0ee32c
Y_Zhong <zhongyuan12345@qq.com>
2020-07-17 17:29:01 +0800

    前端加入情感波动图

--- wwwroot/script/poa.js
+++ wwwroot/script/poa.js
@@ -2,6 +2,8 @@ let official_rank = echarts.init(document.getElementById('official_rank'));
 let weibo_rank = echarts.init(document.getElementById('weibo_rank'));
 let official_senIndex = echarts.init(document.getElementById('official_senIndex'));
 let weibo_senIndex = echarts.init(document.getElementById('weibo_senIndex'));
+let official_senWave = echarts.init(document.getElementById('official_senWave'));
+let weibo_senWave = echarts.init(document.getElementById('weibo_senWave'));
 
 // 数据
 let localPath = '../public';
@@ -10,13 +12,19 @@ let official_rank_legend_data = [];
 let official_rank_series_data = [];
 let official_senIndex_series_data = [];
 let official_senIndex_legend_data = [];
+let official_senWave_series_data = [];
+let official_senWave_legend_data = [];
 // 微博数据
 let weibo_rank_legend_data = [];
 let weibo_rank_series_data = [];
 let weibo_senIndex_series_data = [];
 let weibo_senIndex_legend_data = [];
+let weibo_senWave_series_data = [];
+let weibo_senWave_legend_data = [];
+
 
 let getOfficialData = function () {
+    // 获取TD-IDF排行数据
     let url = localPath + '/official_rank_data.json';
     axios.get(url)
         .then(function (response) {
@@ -30,6 +38,7 @@ let getOfficialData = function () {
             official_rank.setOption(option_official_rank);
         });
 
+    // 获取情感指数数据
     url = localPath + '/official_senIndex_data.json';
     axios.get(url)
         .then(function (response) {
@@ -41,10 +50,25 @@ let getOfficialData = function () {
             }
 
             official_senIndex.setOption(option_official_senIndex);
-        })
+        });
+
+    // 获取情感波动数据
+    url = localPath + '/official_senWave_data.json';
+    axios.get(url)
+        .then(function (response) {
+            let data = response.data;
+
+            for (let i = 0; i < data.series_data.length; i++) {
+                official_senWave_legend_data.push(data.legend_data[i]);
+                official_senWave_series_data.push(data.series_data[i]);
+            }
+
+            official_senWave.setOption(option_official_senWave);
+        });
 };
 
 let getWeiboData = function () {
+    // 获取TD-IDF排行数据
     let url = localPath + '/weibo_rank_data.json';
     axios.get(url)
         .then(function (response) {
@@ -56,8 +80,9 @@ let getWeiboData = function () {
             }
 
             weibo_rank.setOption(option_weibo_rank);
-        })
+        });
 
+    // 获取情感指数数据
     url = localPath + '/weibo_senIndex_data.json';
     axios.get(url)
         .then(function (response) {
@@ -69,7 +94,21 @@ let getWeiboData = function () {
             }
 
             weibo_senIndex.setOption(option_weibo_senIndex);
-        })
+        });
+
+    // 获取情感波动数据
+    url = localPath + '/weibo_senWave_data.json';
+    axios.get(url)
+        .then(function (response) {
+            let data = response.data;
+
+            for (let i = 0; i < data.series_data.length; i++) {
+                weibo_senWave_legend_data.push(data.legend_data[i]);
+                weibo_senWave_series_data.push(data.series_data[i]);
+            }
+
+            weibo_senWave.setOption(option_weibo_senWave);
+        });
 };
 
 let option_official_rank = {
@@ -208,29 +247,119 @@ let option_weibo_senIndex = {
     title: {
         text: '微博情感指数分布',
         subtext: 'January 1 - February 20',
-        left:'right'
+        left: 'right'
     },
     tooltip: {
         trigger: 'item',
     },
     xAxis: {
-        data:weibo_senIndex_legend_data,
+        data: weibo_senIndex_legend_data,
         name: '情感指数',
         axisTick: {
             alignWithLabel: true
         },
-        inverse:true
+        inverse: true,
     },
     yAxis: {
         name: '消息个数',
         nameLocation: 'middle',
         nameGap: 40,
-        position:'right'
+        position: 'right'
     },
     series: [{
         type: 'bar',
         data: weibo_senIndex_series_data,
     }],
+    color: '#F4A460'
+};
+
+let option_official_senWave = {
+    title: {
+        text: '疫情防控平台情感波动',
+        subtext: 'January 26 - February 20',
+    },
+    tooltip: {
+        trigger: 'axis',
+    },
+    xAxis: {
+        type: 'value',
+        position: 'bottom',
+        name: '情感数值'
+    },
+    yAxis: {
+        type: 'category',
+        axisLine: {show: false},
+        axisLabel: {show: true},
+        axisTick: {show: false},
+        splitLine: {show: false},
+        data: official_senWave_legend_data,
+
+    },
+    dataZoom: [
+        {
+            type: 'slider',
+            yAxisIndex: 0,
+            left: '1%'
+        },
+    ],
+    series: [
+        {
+            name: '情感数值',
+            type: 'bar',
+            label: {
+                show: false,
+                formatter: '{b}'
+            },
+            data: official_senWave_series_data,
+            large: true,
+        }
+    ],
+    color: '#48D1CC'
+};
+
+let option_weibo_senWave = {
+    title: {
+        text: '疫情防控平台情感波动',
+        subtext: 'January 1 - February 20',
+        left: 'right'
+    },
+    tooltip: {
+        trigger: 'axis',
+    },
+    xAxis: {
+        type: 'value',
+        position: 'bottom',
+        name: '情感数值',
+        inverse: true,
+    },
+    yAxis: {
+        type: 'category',
+        axisLine: {show: false},
+        axisLabel: {show: true},
+        axisTick: {show: false},
+        splitLine: {show: false},
+        data: weibo_senWave_legend_data,
+        position: 'right'
+    },
+    dataZoom: [
+        {
+            type: 'slider',
+            yAxisIndex: 0,
+            right: '1%'
+        },
+    ],
+    series: [
+        {
+            name: '情感数值',
+            type: 'bar',
+            label: {
+                show: false,
+                formatter: '{b}'
+            },
+            data: weibo_senWave_series_data,
+            large: true,
+        }
+    ],
     color: '#F4A460'
 };
 
@@ -260,15 +389,45 @@ weibo_rank.on('dataZoom', function (params) {
     }
 });
 
+official_senWave.on('dataZoom',function(params){
+    let weibo_start = option_official_senWave.dataZoom[0].start;
+    let weibo_end = option_official_senWave.dataZoom[0].end;
+
+    if (!(weibo_start === params.start && weibo_end === params.end)) {
+        option_weibo_senWave.dataZoom[0].start = params.start;
+        option_weibo_senWave.dataZoom[0].end = params.end;
+
+        weibo_senWave.setOption(option_weibo_senWave)
+    }
+});
+
+weibo_senWave.on('dataZoom', function (params) {
+    let official_start = option_weibo_senWave.dataZoom[0].start;
+    let official_end = option_weibo_senWave.dataZoom[0].end;
+
+    if (!(official_start === params.start && official_end === params.end)) {
+        option_official_senWave.dataZoom[0].start = params.start;
+        option_official_senWave.dataZoom[0].end = params.end;
+
+        official_senWave.setOption(option_official_senWave)
+    }
+});
+
+
+
 function poa_init() {
     official_rank_legend_data.splice(0, official_rank_legend_data.length);
     official_rank_series_data.splice(0, official_rank_series_data.length);
     official_senIndex_series_data.splice(0, official_senIndex_series_data.length);
     official_senIndex_legend_data.splice(0, official_senIndex_legend_data.length);
+    official_senWave_legend_data.splice(0, official_senWave_legend_data.length);
+    official_senWave_series_data.splice(0, official_senWave_series_data.length);
     weibo_rank_legend_data.splice(0, weibo_rank_legend_data.length);
     weibo_rank_series_data.splice(0, weibo_rank_series_data.length);
     weibo_senIndex_legend_data.splice(0, weibo_senIndex_legend_data.length);
     weibo_senIndex_series_data.splice(0, weibo_senIndex_series_data.length);
+    weibo_senWave_legend_data.splice(0, weibo_senWave_legend_data.length);
+    weibo_senWave_series_data.splice(0, weibo_senWave_series_data.length);
 
     getOfficialData();
     getWeiboData();
