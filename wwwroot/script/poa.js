964630f49281830537a4dae0fe0752aa54f2a104
Y_Zhong <zhongyuan12345@qq.com>
2020-07-17 16:58:09 +0800

    添加情感指数分布

--- wwwroot/script/poa.js
+++ wwwroot/script/poa.js
@@ -1,14 +1,20 @@
 let official_rank = echarts.init(document.getElementById('official_rank'));
 let weibo_rank = echarts.init(document.getElementById('weibo_rank'));
+let official_senIndex = echarts.init(document.getElementById('official_senIndex'));
+let weibo_senIndex = echarts.init(document.getElementById('weibo_senIndex'));
 
 // 数据
 let localPath = '../public';
 // 疫情防控平台官方数据
 let official_rank_legend_data = [];
 let official_rank_series_data = [];
+let official_senIndex_series_data = [];
+let official_senIndex_legend_data = [];
 // 微博数据
 let weibo_rank_legend_data = [];
 let weibo_rank_series_data = [];
+let weibo_senIndex_series_data = [];
+let weibo_senIndex_legend_data = [];
 
 let getOfficialData = function () {
     let url = localPath + '/official_rank_data.json';
@@ -22,6 +28,19 @@ let getOfficialData = function () {
             }
 
             official_rank.setOption(option_official_rank);
+        });
+
+    url = localPath + '/official_senIndex_data.json';
+    axios.get(url)
+        .then(function (response) {
+            let data = response.data;
+
+            for (let i = 0; i < data.series_data.length; i++) {
+                official_senIndex_legend_data.push(data.legend_data[i]);
+                official_senIndex_series_data.push(data.series_data[i]);
+            }
+
+            official_senIndex.setOption(option_official_senIndex);
         })
 };
 
@@ -38,13 +57,26 @@ let getWeiboData = function () {
 
             weibo_rank.setOption(option_weibo_rank);
         })
+
+    url = localPath + '/weibo_senIndex_data.json';
+    axios.get(url)
+        .then(function (response) {
+            let data = response.data;
+
+            for (let i = 0; i < data.series_data.length; i++) {
+                weibo_senIndex_legend_data.push(data.legend_data[i]);
+                weibo_senIndex_series_data.push(data.series_data[i]);
+            }
+
+            weibo_senIndex.setOption(option_weibo_senIndex);
+        })
 };
 
 let option_official_rank = {
     title: [
         {
             text: "疫情防控平台 TF-IDF Ranking",
-            subtext: 'January 26 - July 10',
+            subtext: 'January 26 - February 20',
         }
     ],
     grid: {
@@ -96,6 +128,7 @@ let option_weibo_rank = {
     title: [
         {
             text: "新浪微博 TF-IDF Ranking",
+            subtext: 'January 1 - February 20',
             left: 'right'
         }
     ],
@@ -144,6 +177,64 @@ let option_weibo_rank = {
     color: '#F4A460'
 };
 
+let option_official_senIndex = {
+    title: {
+        text: '疫情防控平台情感指数分布',
+        subtext: 'January 26 - February 20'
+    },
+    tooltip: {
+        trigger: 'item',
+    },
+    xAxis: {
+        data: official_senIndex_legend_data,
+        name: '情感指数',
+        axisTick: {
+            alignWithLabel: true
+        },
+    },
+    yAxis: {
+        name: '消息个数',
+        nameLocation: 'middle',
+        nameGap: 40
+    },
+    series: [{
+        type: 'bar',
+        data: official_senIndex_series_data,
+    }],
+    color: '#48D1CC'
+};
+
+let option_weibo_senIndex = {
+    title: {
+        text: '微博情感指数分布',
+        subtext: 'January 1 - February 20',
+        left:'right'
+    },
+    tooltip: {
+        trigger: 'item',
+    },
+    xAxis: {
+        data:weibo_senIndex_legend_data,
+        name: '情感指数',
+        axisTick: {
+            alignWithLabel: true
+        },
+        inverse:true
+    },
+    yAxis: {
+        name: '消息个数',
+        nameLocation: 'middle',
+        nameGap: 40,
+        position:'right'
+    },
+    series: [{
+        type: 'bar',
+        data: weibo_senIndex_series_data,
+    }],
+    color: '#F4A460'
+};
+
+
 // 事件
 official_rank.on('dataZoom', function (params) {
     let weibo_start = option_weibo_rank.dataZoom[0].start;
@@ -172,8 +263,12 @@ weibo_rank.on('dataZoom', function (params) {
 function poa_init() {
     official_rank_legend_data.splice(0, official_rank_legend_data.length);
     official_rank_series_data.splice(0, official_rank_series_data.length);
+    official_senIndex_series_data.splice(0, official_senIndex_series_data.length);
+    official_senIndex_legend_data.splice(0, official_senIndex_legend_data.length);
     weibo_rank_legend_data.splice(0, weibo_rank_legend_data.length);
     weibo_rank_series_data.splice(0, weibo_rank_series_data.length);
+    weibo_senIndex_legend_data.splice(0, weibo_senIndex_legend_data.length);
+    weibo_senIndex_series_data.splice(0, weibo_senIndex_series_data.length);
 
     getOfficialData();
     getWeiboData();
